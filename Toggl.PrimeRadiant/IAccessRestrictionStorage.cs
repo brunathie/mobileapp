073888a8ac883054e85a9027f9ac6a0d9c14f185
simonrozsival <simon@rozsival.com>
2017-11-22 08:54:59 +0100

    :sparkles: Add access resttriction storage interface

---
+++ Toggl.PrimeRadiant/IAccessRestrictionStorage.cs
@@ -0,0 +1,13 @@
+﻿namespace Toggl.PrimeRadiant
+{
+    public interface IAccessRestrictionStorage
+    {
+        void SetOutdatedClientVersion();
+        void SetOutdatedApiVersion();
+        void SetUnauthorizedAccess();
+        bool IsAccessRestricted();
+        bool IsClientOutdated();
+        bool IsApiOutdated();
+        bool IsUnauthorized();
+    }
+}
