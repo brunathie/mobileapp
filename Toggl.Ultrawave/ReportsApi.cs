9ded7ffece2b7d7eb6b29f8dc13b16f37e02ff31
Simon Rozsival <simon@rozsival.com>
2017-11-24 16:06:08 +0100

    :sparkles: Add reports api clients container

---
+++ Toggl.Ultrawave/ReportsApi.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using ModernHttpClient;
+using Toggl.Multivac;
+using Toggl.Ultrawave.Network;
+using Toggl.Ultrawave.ReportsApiClients;
+using Toggl.Ultrawave.Serialization;
+using static System.Net.DecompressionMethods;
+
+namespace Toggl.Ultrawave
+{
+    public class ReportsApi
+    {
+        public ReportsApi(ApiConfiguration configuration, HttpClientHandler handler = null)
+        {
+            Ensure.Argument.IsNotNull(configuration, nameof(configuration));
+
+            var httpHandler = handler ?? new NativeMessageHandler { AutomaticDecompression = GZip | Deflate };
+            var httpClient = new HttpClient(httpHandler);
+
+            var userAgent = configuration.UserAgent;
+            var credentials = configuration.Credentials;
+            var serializer = new JsonSerializer();
+            var apiClient = new ApiClient(httpClient, userAgent);
+            var endpoints = new ReportsEndpoints(configuration.Environment);
+
+            ProjectsSummary = new ProjectsSummaryApi(endpoints.ProjectsSummary, apiClient, serializer, credentials);
+        }
+
+        public IProjectsSummaryApi ProjectsSummary { get; }
+    }
+}
