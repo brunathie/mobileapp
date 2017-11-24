288d42bf7745ea2d7396babca8991ae066d65af5
Simon Rozsival <simon@rozsival.com>
2017-11-24 16:05:45 +0100

    :sparkles: Add reports api client

---
+++ Toggl.Ultrawave/ReportsApiClients/Interfaces/IProjectsSummaryApi.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Toggl.Multivac.ReportsModels;
+
+namespace Toggl.Ultrawave.ReportsApiClients
+{
+    public interface IProjectsSummaryApi
+    {
+        IObservable<IProjectsSummary> GetByWorkspace(long workspaceId, DateTimeOffset startDate, DateTimeOffset? endDate);
+    }
+}
---
+++ Toggl.Ultrawave/ReportsApiClients/ProjectsSummaryApi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reactive.Linq;
+using Toggl.Multivac.ReportsModels;
+using Toggl.Ultrawave.ApiClients;
+using Toggl.Ultrawave.Network;
+using Toggl.Ultrawave.Serialization;
+using Toggl.Ultrawave.ReportsModels;
+
+namespace Toggl.Ultrawave.ReportsApiClients
+{
+    internal sealed class ProjectsSummaryApi : BaseApi, IProjectsSummaryApi
+    {
+        private readonly ProjectsSummaryEndpoints endPoints;
+        private readonly IJsonSerializer serializer;
+        private readonly Credentials credentials;
+
+        public ProjectsSummaryApi(ProjectsSummaryEndpoints endPoints, IApiClient apiClient, IJsonSerializer serializer, Credentials credentials)
+            : base(apiClient, serializer, credentials)
+        {
+            this.endPoints = endPoints;
+            this.serializer = serializer;
+            this.credentials = credentials;
+        }
+
+        public IObservable<IProjectsSummary> GetByWorkspace(long workspaceId, DateTimeOffset startDate, DateTimeOffset? endDate)
+        {
+            var parameters = new ProjectsSummaryParameters { StartDate = startDate, EndDate = endDate };
+            var json = serializer.Serialize(parameters, SerializationReason.Post, null);
+            return Observable.Create<IProjectsSummary>(async observer =>
+            {
+                var projectsSummaries = await CreateListObservable<ProjectSummary, IProjectSummary>(endPoints.Post(workspaceId), credentials.Header, json);
+                var summary = new ProjectsSummary { StartDate = startDate, EndDate = endDate, ProjectsSummaries = projectsSummaries };
+
+                observer.OnNext(summary);
+                observer.OnCompleted();
+
+                return () => { };
+            });
+        }
+
+        private sealed class ProjectsSummaryParameters
+        {
+            public DateTimeOffset StartDate { get; set; }
+            public DateTimeOffset? EndDate { get; set; }
+        }
+    }
+}
