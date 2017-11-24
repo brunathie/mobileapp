bc3c304fd4cd7ea8d00f7d745d1748bdec17e265
Simon Rozsival <simon@rozsival.com>
2017-11-24 18:01:57 +0100

    :white_check_mark: Add more serialization tests

---
+++ Toggl.Ultrawave.Tests/ReportsModels/ProjectsSummaryTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Toggl.Ultrawave.Models;
+using Toggl.Ultrawave.ReportsModels;
+using Xunit;
+
+namespace Toggl.Ultrawave.Tests.Models
+{
+    public sealed class ProjectsSummaryTests
+    {
+        public sealed class TheProjectsSummaryModel
+        {
+            private string validJson
+                => "[{\"user_id\":23741667,\"project_id\":1427273,\"tracked_seconds\":9876,\"billed_seconds\":6543},"
+                    + "{\"user_id\":23741667,\"project_id\":1427273,\"tracked_seconds\":5678,\"billed_seconds\":1029},"
+                    + "{\"user_id\":23741667,\"project_id\":1427273,\"tracked_seconds\":4598,\"billed_seconds\":56}]";
+
+            private List<ProjectSummary> validSummary => new List<ProjectSummary>
+            {
+                new ProjectSummary
+                {
+                    UserId = 23741667,
+                    ProjectId = 1427273,
+                    TrackedSeconds = 9876,
+                    BilledSeconds = 6543
+                },
+                new ProjectSummary
+                {
+                    UserId = 23741667,
+                    ProjectId = 1427273,
+                    TrackedSeconds = 5678,
+                    BilledSeconds = 1029
+                },
+                new ProjectSummary
+                {
+                    UserId = 23741667,
+                    ProjectId = 1427273,
+                    TrackedSeconds = 4598,
+                    BilledSeconds = 56
+                }
+            };
+
+            [Fact, LogIfTooSlow]
+            public void CanBeDeserialized()
+            {
+                SerializationHelper.CanBeDeserialized(validJson, validSummary);
+            }
+
+            [Fact, LogIfTooSlow]
+            public void CanBeSerialized()
+            {
+                SerializationHelper.CanBeSerialized(validJson, validSummary);
+            }
+        }
+    }
+}
