6282484b09ecc4184f033d9bfadd2f878666a239
simonrozsival <simon@rozsival.com>
2017-11-27 09:38:39 +0100

    :white_check_mark: Add basic integration test

---
+++ Toggl.Ultrawave.Tests.Integration/ProjectsSummaryReportsApiTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reactive.Linq;
+using Toggl.Multivac.ReportsModels;
+using Toggl.Ultrawave.Tests.Integration.BaseTests;
+
+namespace Toggl.Ultrawave.Tests.Integration
+{
+    public sealed class ProjectsSummaryReportsApiTests
+    {
+        public sealed class TheGetByWorkspaceMethod : AuthenticatedEndpointBaseTests<IProjectsSummary>
+        {
+            protected override IObservable<IProjectsSummary> CallEndpointWith(ITogglApi togglApi)
+            {
+                var from = DateTimeOffset.UtcNow.AddDays(-2);
+                var to = DateTimeOffset.UtcNow.AddDays(-1);
+
+                return ValidApi.User
+                    .Get()
+                    .SelectMany(user =>
+                        togglApi.ReportsApi.ProjectsSummary.GetByWorkspace(user.DefaultWorkspaceId, from, to));
+            }
+        }
+    }
+}
